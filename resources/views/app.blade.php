1360245c4233912ffa64b4a916d115742175afdb
Coby Fielding <cobyfielding@hotmail.com>
2016-02-19 15:53:22 -0800

    bs

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,10 @@
 <head>
     <title>Laravel</title>
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+    <link rel="stylesheet" href="css/main.css">
 
     <style>
         html, body {
